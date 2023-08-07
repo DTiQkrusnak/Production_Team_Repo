56a0863fa1f21539188613670c5c4f850941a4b4
Denis Bochon <114193188+denisbochon@users.noreply.github.com>
2023-08-07 13:23:18 +0200

    add non-blocking sql module

---
+++ Powershell/NuGet-Modules/SQL-Utilities/SQL-Utilities.psm1
@@ -0,0 +1,20 @@
+function Invoke-SqlQuery {
+    param (
+        [Parameter(Mandatory)] [string] $Server,
+        [Parameter(Mandatory)] [string] $Username,
+        [Parameter(Mandatory)] [Securestring] $Password,
+        [Parameter(Mandatory)] [string] $Query,
+        [int] $Timeout = 10
+    )
+
+    # Clear possible connections that are still open to ensure that Invoke-Sqlcmd  succeeds
+    [System.Data.SqlClient.SqlConnection]::ClearAllPools()
+    try {
+        $result = Invoke-Sqlcmd -ServerInstance $Server -Username $Username -Password $Password -Query $Query -TrustServerCertificate -ConnectionTimeout $Timeout -ErrorAction Stop
+        # Clear connection that was made with Invoke-Sqlcmd
+        [System.Data.SqlClient.SqlConnection]::ClearAllPools()
+        return $result
+    } catch {
+        return $_
+    }
+}
