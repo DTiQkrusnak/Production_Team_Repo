cb279e8111545268a6a341df268c65b55048f442
Denis Bochon <114193188+denisbochon@users.noreply.github.com>
2023-08-09 10:27:21 +0200

    Add removeApp function as fallback
    Function provided in pull conversation added as fallback in
    case when Uninstall-Package cmdlet will not remove app.

--- Powershell/Production_fixes/UninstallPVMController/src/UninstallPVMController.ps1
+++ Powershell/Production_fixes/UninstallPVMController/src/UninstallPVMController.ps1
@@ -1,3 +1,36 @@
+function removeApp {
+    param (
+        [string[]] $AppsToRemove
+    )
+
+    $RegKeys = @(
+        'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+        'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\'
+    )
+
+    $Apps = $RegKeys |
+    Get-ChildItem |
+    Get-ItemProperty |
+    Where-Object { $AppsToRemove -contains $_.DisplayName -and $_.UninstallString }
+
+    if (!($Apps)) {
+        Write-Output('Apps not found - probably already uninstalled, exiting script')
+        exit
+    }
+
+    foreach ( $App in $Apps ) {
+        $UninstallString =
+        if ( $App.Uninstallstring -match 'MsiExec.exe' ) {
+            "$( $App.UninstallString -replace '/I', '/X ' ) /qn /norestart"
+        }
+        else {
+            $App.UninstallString + " /S"
+        }
+        Write-Output("Uninstalling app: $UninstallString")
+        Start-Process -FilePath cmd -ArgumentList '/c', $UninstallString -NoNewWindow -Wait
+    }
+}
+
 
 function uninstallApplication {
     param (
@@ -9,10 +42,16 @@ function uninstallApplication {
             Uninstall-Package -Name $Name -Confirm -Force
             Write-Output("Removed app: $Name")
         }
+        if (Get-Package -ProviderName Programs -IncludeWindowsInstaller -Name $Name -ErrorAction Stop) {
+            removeApp -AppsToRemove $Name
+            Write-Output("Removed app: $Name")
+        }
     } catch {
         Write-Output('Application package not found')
     }
 }
+
+
 
 uninstallApplication -Name '360iQPVMController'
 try {
