132bc0a626fbf940dc28b799cbe906af3ecb60ab
Denis Bochon <114193188+denisbochon@users.noreply.github.com>
2023-08-09 12:15:45 +0200

    Add repo registration to script template
    Script template now includes repo registration to point to
    shellget.go360iq.com NuGet server for modules.

--- Powershell/Templates/ScriptTemplate.ps1
+++ Powershell/Templates/ScriptTemplate.ps1
@@ -23,6 +23,7 @@ function executeScript {
 	)
 	if ($PShellVer -ge 5) {
 		Write-Output("Powershell.v.5 found - executing script")
+		Register-PSRepository -Name 'ShellGet' -SourceLocation 'https://shellget.go360iq.com/nuget' -InstallationPolicy Trusted -ErrorAction SilentlyContinue
 	}
 	else {
 		Write-Output("PowerShell.v.5 is not installed - skipping script ")
