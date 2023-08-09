c7735c896962bc90554f2ead3bfd09a8b293b3d4
Denis Bochon <114193188+denisbochon@users.noreply.github.com>
2023-08-09 09:40:12 +0200

    Add breeze module
    breeze module that exports endpoint address variable which
    can be updated to keep other scripts up to date.

---
+++ Powershell/NuGet-Modules/Breeze/Breeze.psm1
@@ -0,0 +1,5 @@
+[string] $BreezeAddress = "https://p13fqdhy8i.execute-api.us-east-1.amazonaws.com/prod/ScriptExecution"
+
+# For future might be good idea to create function that implements Breeze template to lessen boilerplate code copy/pasting
+
+Export-ModuleMember -Variable 'BreezeAddress'
