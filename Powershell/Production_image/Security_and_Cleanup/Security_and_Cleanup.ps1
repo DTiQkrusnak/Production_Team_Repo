f8432f9b1fe90d1bfba0098b5b90557555da42a6
Denis Bochon <114193188+denisbochon@users.noreply.github.com>
2023-08-18 10:32:02 +0200

    Add Atera Install, documentation
    AteraInstall function WIP which currently downloads files,
    checks controllerID and Model from DB

--- Powershell/Production_image/Security_and_Cleanup/Security_and_Cleanup.ps1
+++ Powershell/Production_image/Security_and_Cleanup/Security_and_Cleanup.ps1
@@ -1,7 +1,11 @@
+[Net.ServicePointManager]::SecurityProtocol = 'TLS12', 'SSL3'
 $script:errorArray = @()
+
+
 function blockWin11Upgrade () {
 	<#
-    .DESCRIPTION Blocks Windows 11 upgrade prompts and blocks version on 21H2
+    .DESCRIPTION
+	Blocks Windows 11 upgrade prompts and blocks version on 21H2
     #>
 
 	$regPath = 'Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate'
@@ -12,7 +16,7 @@ function blockWin11Upgrade () {
 		if (!(Test-path $regPath)) {
 			New-Item -Path $regPath -Force
 		}
-    
+
 		New-ItemProperty -Path $regPath -Name "ProductVersion" -value "Windows 10" -PropertyType String -Force | Out-Null
 		New-ItemProperty -Path $regPath -Name "TargetReleaseVersion" -value 1 -PropertyType DWord -Force | Out-Null
 		New-ItemProperty -Path $regpath -Name "TargetReleaseVersionInfo" -value "21H2" -PropertyType String -Force | Out-Null
@@ -23,10 +27,15 @@ function blockWin11Upgrade () {
 }
 
 function DisableWinUpdateIfAteraNotExists () {
+	<#
+	.DESCRIPTION
+	Disable Windows Update service if Atera service is not present in the system
+	#>
 	$ateraRegistryKey = $winUpdateService = $null
 	$ateraRegistryKey = Get-Item "registry::HKEY_LOCAL_MACHINE\SOFTWARE\ATERA Networks\AlphaAgent" -ErrorAction SilentlyContinue
 	$ateraService = Get-Service -Name 'AteraAgent' -ErrorAction SilentlyContinue
-    
+
+	# Checking if atera is present
 	if (($null -ne $ateraRegistryKey) -and ($null -ne $ateraService)) {
 		Write-Output("Atera installed - nothing to do.")
 		Write-Output("Registery keys check : $ateraRegistryKey")
@@ -42,5 +51,48 @@ function DisableWinUpdateIfAteraNotExists () {
 		Write-Output("Stopping windows update service")
 		$winUpdateService | Stop-Service -Force -ErrorAction SilentlyContinue
 		Write-Output("    -> $($winUpdateService.DisplayName) service stopped")
-	} 
+		
+		<#
+		CHECK if 'BITS' and 'DoSvc' has to be disabled here as well
+		#>
+	}
+}
+
+function AteraInstall () {
+	Write-Output("ATERA INSTALL")
+	# Check if folder for downloads exists
+	$path = 'C:\ProgramData\DTiQ\TaskScheduler\ateraInstall'
+	if ((Test-Path $path) -eq $false) {
+        Write-Output("Creating directory : $($path)")
+        New-Item $path -ItemType Directory -Force | Out-Null
+        Write-Output("    -> done")
+    } else {
+		Write-Output("Download path exists.")
+	}
+	
+	# Download files
+	$path = 'C:\ProgramData\DTiQ\TaskScheduler\ateraInstall'
+	Invoke-WebRequest -Uri "https://files-us-ps2.go360iq.com/_Files/Software/Scripts/ateraInstall/atera.exe" -OutFile "$path\atera.exe" -Verbose
+	Invoke-WebRequest -Uri "https://files-us-ps2.go360iq.com/_Files/Software/Scripts/ateraInstall/setup_final.msi" -OutFile "$path\setup_final.msi" -Verbose
+	Write-Output("Files downloaded")
+
+	# Get ControllerId from database EZ360Objects
+	$getControllerIdQuery = @'
+	SELECT TOP 1 *
+	FROM [EZ360Objects].[Location].[Controllers]
+	WHERE [Status] = 'Y'
+'@
+	$contrllerId = (Invoke-Sqlcmd -server '.\EZ360' -Query $getControllerIdQuery -QueryTimeout 30 -Database 'EZ360Objects' -Username 'EZ360System' -Password 'EZ360System').ControllerId
+	[System.Data.SqlClient.SqlConnection]::ClearAllPools()
+
+	# Get Controller Model from database EZ360Objects
+	$getControllerModelQuery = @'
+    SELECT CM.Name
+    FROM Location.Controllers AS LC
+    LEFT JOIN Controller.Models AS CM
+    ON CM.ModelID = LC.ModelID
+    WHERE LC.Status = 'Y'
+'@
+    $controllerModel = (Invoke-Sqlcmd -server '.\EZ360' -Query $getControllerModelQuery -Database 'EZ360Objects' -Username 'EZ360System' -Password 'EZ360System' -QueryTimeout 30).name
+
 }
