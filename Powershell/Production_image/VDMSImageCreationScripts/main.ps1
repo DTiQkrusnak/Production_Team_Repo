edf381005ce600eae325e0ad4fccd04bacf46b90
Denis Bochon <denisbochon@outlook.com>
2023-10-10 22:50:39 +0200

    IM-48 Add elevation check
    - Add function to check if script is running as user in Administrators
    group

--- Powershell/Production_image/VDMSImageCreationScripts/main.ps1
+++ Powershell/Production_image/VDMSImageCreationScripts/main.ps1
@@ -2,22 +2,30 @@ $scriptsLocation = $PSScriptRoot + "\scripts"
 $logsLocation = $PSScriptRoot + "\scripts\logs\"
 $scriptsCollection = Get-ChildItem -Path $scriptsLocation\*.ps1 | Sort-Object -Property Name
 
+function checkAdministratorElevation() {
+	try {
+		return (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+	} catch {
+		Write-Error('[-] Cannot return elevation status')
+	}
+}
+
 function createLogsDir {
-    if (!(Test-Path $logsLocation)) {
-        New-Item -Path $scriptsLocation -ItemType Directory -Name logs | Out-Null
-        if (!(Test-Path $logsLocation\main.log)) {
-            New-Item -Path $scriptsLocation\logs -ItemType File -Name main.log | Out-Null
-        }
-    }
+	if (!(Test-Path $logsLocation)) {
+		New-Item -Path $scriptsLocation -ItemType Directory -Name logs | Out-Null
+		if (!(Test-Path $logsLocation\main.log)) {
+			New-Item -Path $scriptsLocation\logs -ItemType File -Name main.log | Out-Null
+		}
+	}
 }
 function executeScripts {
-    foreach ($script in $scriptsCollection) {
-        Write-output "$(Get-Date -format 'u') - ========> Log start : $script" | Tee-Object -Append $scriptsLocation\logs\main.log
-            powershell -noprofile -executionpolicy bypass -file $script | Tee-Object -Append $scriptsLocation\logs\main.log
-        Write-output "$(Get-Date -format 'u') - ========> Log end : $script" | Tee-Object -Append $scriptsLocation\logs\main.log
-        Write-output "" | Tee-Object -Append $scriptsLocation\logs\main.log
-        #$script
-    }    
+	foreach ($script in $scriptsCollection) {
+		Write-output "$(Get-Date -format 'u') - ========> Log start : $script" | Tee-Object -Append $scriptsLocation\logs\main.log
+		powershell -noprofile -executionpolicy bypass -file $script | Tee-Object -Append $scriptsLocation\logs\main.log
+		Write-output "$(Get-Date -format 'u') - ========> Log end : $script" | Tee-Object -Append $scriptsLocation\logs\main.log
+		Write-output "" | Tee-Object -Append $scriptsLocation\logs\main.log
+		#$script
+	}
 }
 
 createLogsDir
