8831f1c573f4b290cb32810140fe5dbff4328486
Denis Bochon <denisbochon@outlook.com>
2023-12-12 15:29:05 +0100

    Add trust server cert, rework restart synchronizer

--- Powershell/Production_fixes/ToViaSynchronizer/Disable.ps1
+++ Powershell/Production_fixes/ToViaSynchronizer/Disable.ps1
@@ -13,7 +13,7 @@ function checkDBconnection {
 	$DBServer = ".\\EZ360"
 	$databasename = "EZ360Controllers"
 	$Connection = New-Object system.data.sqlclient.sqlconnection
-	$Connection.ConnectionString = "server=$DBServer;database=$databasename;uid=EZ360System;password=EZ360System;"
+	$Connection.ConnectionString = "server=$DBServer;database=$databasename;uid=EZ360System;password=EZ360System;TrustServerCertificate=True"
 
 	try {
 		$Connection.Open()
@@ -45,61 +45,15 @@ function executeSqlV2 {
 
 }
 
-
-# General function to check if Synchronizer is running on target system
-function CheckIfSynchronizerIsRunning {
-	try {
-		if ((Get-Service -Name "EZ360ControllerSynchronizer").Status -eq 'Running') {
-			return $True
-		}
-		else {
-			return $False
-		}
-	} catch {
-		return $False
-	}
-}
-
 function restartSynchronizer {
-	# Multiple ways to restart Synchronizer after script execution
-	Restart-Service -Name 'EZ360ControllerSynchronizer' -ErrorAction SilentlyContinue
-	if (CheckIfSynchronizerIsRunning -eq $False) {
-		Write-Output('Restarted Synchronizer')
-		exit 0
-	}
-	Restart-Service -Name 'EZ360ControllerSynchronizer' -Force -ErrorAction SilentlyContinue
-	if (CheckIfSynchronizerIsRunning -eq $False) {
-		Write-Output('Restarted Synchronizer with force parameter')
-		exit 0
-	}
 	try {
-		$processId = (Get-Process -Name 'EZ360ControllerSynchronizer.Service' -ErrorAction SilentlyContinue).Id 
-		Stop-Process -Id $processId -Force -ErrorAction Stop
-		Start-Service -Name 'EZ360ControllerSynchronizer' -ErrorAction SilentlyContinue
+		$synchronizerService = Get-Service -Name 'EZ360ControllerSynchronizer' -ErrorAction Stop
 	} catch {
-		Write-Output('Cannot stop Synchronizer process')
+		$_.Exception.Message
 	}
-	if (CheckIfSynchronizerIsRunning -eq $False) {
-		Write-Output('Restarted Synchronizer with process stopping and service start')
-		exit 0
-	}
-	if (CheckIfSynchronizerIsRunning -eq $True) {
-		try {
-			$status = taskkill /f /im 'EZ360ControllerSynchronizer.Service'
-			if ($status.Contains('SUCCESS')) {
-				Write-Output('Killed Synchronizer, service was not reponding to restart signal')
-				exit 0
-			}
-			else {
-				throw 'Cannot kill Synchronizer service'
-				exit -10
-			}
-		}
-		catch {
-			Write-Error($_.Exception.Message)
-			exit -10
-		}
-	}
+
+	$synchronizerService.Stop()
+	$synchronizerService.WaitForStatus('Stopped', '00:00:30')
 }
 
 $connectionBool = checkDBconnection
