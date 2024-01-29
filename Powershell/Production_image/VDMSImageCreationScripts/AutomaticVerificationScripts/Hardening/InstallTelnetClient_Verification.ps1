c693668c8307667b09cb55e0e36c0ffdf2df0a92
Denis Bochon <denisbochon@outlook.com>
2024-01-29 09:13:12 +0100

    Move telnet to prerequisites

--- Powershell/Production_image/VDMSImageCreationScripts/AutomaticVerificationScripts/Hardening/InstallTelnetClient_Verification.ps1
+++
@@ -1,7 +0,0 @@
-if ('Enabled' -eq (Get-WindowsOptionalFeature -Online -FeatureName 'TelnetClient').State) {
-    Write-Host " [PASSED] " -NoNewline -ForegroundColor Green
-    Write-Host " Telnet Client is installed"
-} else {
-    Write-Host " [FAILED] " -NoNewline -ForegroundColor Red
-    Write-Host " Telnet Client is disabled "
-}
--- Powershell/Production_image/VDMSImageCreationScripts/Hardening/InstallTelnetClient.ps1
+++
@@ -1,6 +0,0 @@
-try {
-    Enable-WindowsOptionalFeature -Online -FeatureName 'TelnetClient' -NoRestart -All -ErrorAction Stop
-} catch {
-    Write-Output($_.Exception.Message)
-    Write-Output("Line: $($_.InvocationInfo.ScriptLineNumber)")
-}
