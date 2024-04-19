797e6f306211bc4f6d9615bfe0b633bc117d12d1
karol.rusnak <krusnak@dtiq.com>
2024-04-19 12:30:31 +0200

    added TLS 1.2 protocol so the SQLServer module can be downloaded

--- Powershell/Production_fixes/PVM_ShowControlls/PVM_ShowControlls.ps1
+++ Powershell/Production_fixes/PVM_ShowControlls/PVM_ShowControlls.ps1
@@ -1,10 +1,13 @@
-$scriptVer = "1.2"
+$scriptVer = "1.3"
 $scriptName = "pvm_showControlsFix"
 $scriptDescr = "insert ShowControls depending on json configuration"
 Write-Output("SCRIPT DESCRIPTION: $scriptName v.$scriptVer")
 Write-Output("SCRIPT DESCRIPTION: $scriptDescr")
 
 <#
+    .VERSION_1.3
+    - added TLS 1.2 protocol so the SQLServer module can be downloaded
+
     .VERSION_1.2
     - fixed database configuration check, now it will properly detect that configuration in db is absent
     - new approach on reinstaling sqlserver module
@@ -17,6 +20,7 @@ Write-Output("SCRIPT DESCRIPTION: $scriptDescr")
 #>
 
 #### Variables
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $PShellVer = $PSVersionTable.PSVersion.Major
 $indexControllsObject = New-Object System.Collections.Generic.List[PSCustomObject]
 $sqlModuleName = Get-Module -Name "SQLServer" -ErrorAction SilentlyContinue
@@ -30,7 +34,7 @@ if (!$getPSGalleryRepo) {
 }
 
 if ($sqlModuleName.Version -ne $sqlModuleVersion) {
-    Uninstall-Module -Name SQLServer -AllVersions -Force
+    Uninstall-Module -Name SQLServer -AllVersions -Force -ErrorAction SilentlyContinue+
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
     
     Write-Host "Installing $($sqlModuleName.Name) module..."    
