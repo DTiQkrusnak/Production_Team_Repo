7137c5d9978b4dfd25c94c4ec9971d0f500d435b
Denis Bochon <denisbochon@outlook.com>
2024-05-06 14:10:30 +0200

    Nuget-Modules push

---
+++ Powershell/NuGet-Modules/Expand-ArchiveWithFallback/Expand-ArchiveWithFallback.psm1
@@ -0,0 +1,26 @@
+function Expand-ArchiveWithFallback {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({Test-Path -Path $_ -PathType Leaf })]
+        [ValidateScript({ (Split-Path $_ -Leaf).EndsWith('.zip')})] [string] $Path,
+        [ValidateScript({Test-Path -Path $_ -PathType Container -IsValid})] [string] $DestinationPath = $null
+    )
+
+    if (!$DestinationPath) {
+        $DestinationPath = $Path -replace ('.zip', '')
+    }
+
+    try {
+        if (!(Test-Path -Path $DestinationPath)) {
+            New-Item -Name ((Split-Path -Path $DestinationPath -Leaf) -replace ('.zip','')) -Force -ItemType Container | Out-Null
+        }
+        Expand-Archive -Path $Path -DestinationPath $DestinationPath -Force
+    } catch {
+        try {
+            Remove-Item -Path $DestinationPath -Force -Recurse -Confirm:$false -ErrorAction SilentlyContinue
+            [System.IO.Compression.ZipFile]::ExtractToDirectory( $Path, $DestinationPath)
+        } catch {
+            throw $_
+        }
+    }
+}
---
+++ Powershell/NuGet-Modules/Invoke-DownloadAndVerify/Invoke-DownloadAndVerify.psm1
@@ -0,0 +1,69 @@
+class FileDownloadInformation {
+    [ValidateNotNullOrEmpty()][string] $Name
+    [ValidateNotNullOrEmpty()][string] $Link
+    [ValidateNotNullOrEmpty()][string] $SHA256Hash
+    [boolean] $Success = $false
+    [string] $SavedAtPath = $null
+}
+
+function New-FileDownloadInformation {
+    param (
+        [ValidateNotNullOrEmpty()][string] $Name,
+        [ValidateNotNullOrEmpty()][string] $Link,
+        [ValidateNotNullOrEmpty()][string] $SHA256Hash
+    )
+    $tempFileDownloadInformationObject = [FileDownloadInformation]::new()
+    $tempFileDownloadInformationObject.Name = $Name
+    $tempFileDownloadInformationObject.Link = $Link
+    $tempFileDownloadInformationObject.SHA256Hash = $SHA256Hash
+    return $tempFileDownloadInformationObject
+}
+
+function Invoke-DownloadAndVerify {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$True)]
+        [ValidateScript({Test-Path -Path $_ -PathType Container})]
+        [string] $DownloadPath,
+
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [FileDownloadInformation] $FileInfo,
+
+        [Int32] $Timeout = 30
+    )
+
+    function Test-ForExistingFile([FileDownloadInformation] $FileInfo, [string] $DownloadPath ) {
+        if (Test-Path -Path $FileInfo.Name -PathType Leaf) {
+            $FileInfo.SavedAtPath = $DownloadPath
+            $downloadHash = Get-FileHash -Path $FileInfo.Name -Algorithm SHA256
+            if ($downloadHash.Hash -eq $FileInfo.SHA256Hash) {
+                $FileInfo.Success = $true
+                return $true
+            } else {
+                Write-Error("Hash mismatch for $($FileInfo.Name). Download: $($downloadHash.Hash), Expected: $($FileInfo.SHA256Hash)")
+                return $false
+            }
+        } else {
+            return $false
+        }
+    }
+
+    try {
+        Push-Location -LiteralPath $DownloadPath
+        if (Test-ForExistingFile -FileInfo $FileInfo -DownloadPath $DownloadPath) {
+            return
+        } else {
+            Invoke-WebRequest -Uri $FileInfo.Link -OutFile $FileInfo.Name -TimeoutSec $Timeout -ErrorAction Stop
+            if (Test-ForExistingFile -FileInfo $FileInfo -DownloadPath $DownloadPath) {
+                return
+            } else {
+                Write-Error("Cannot download $FileInfo")
+            }
+        }
+    } catch {
+        throw $_
+    } finally {
+        Pop-Location
+    }
+}
